78d079ad2cd0efc52fd3330b4863b3653474eb9e
owenramber1989 <owenramber1989@163.com>
2024-11-28 22:07:08 +0800

    feat: impl message view

---
+++ simplechat/MessageView.swift
@@ -0,0 +1,65 @@
+//
+//  MessageView.swift
+//  simplechat
+//
+//  Created by 张晋铭 on 2024/11/28.
+//
+
+import SwiftUI
+
+struct Message : Decodable {
+    let uid: String
+    let text: String
+    let photoURL: String
+    let createdAt: Date
+    
+    func isFromCurrentUser() -> Bool {
+        return true
+    }
+}
+
+struct MessageView: View {
+    var message: Message
+    var body: some View {
+        if message.isFromCurrentUser() {
+            HStack {
+                HStack {
+                    Text(message.text)
+                        .padding()
+                }
+                .frame(maxWidth: 250, alignment: .topLeading)
+                .background(.blue)
+                .cornerRadius(20)
+                
+                Image(uiImage: UIImage(named:  "Asuka_Langley_Soryu")!)
+                    .resizable()  // 使图片可调整大小
+                        .aspectRatio(contentMode: .fit)  // 保持原始纵横比并填充指定的框架
+                    .frame(maxWidth: 80, alignment: .top)
+                    .padding(.bottom, 16)
+                    .padding(.leading, 4)
+                    .cornerRadius(20)
+            }
+            .frame(maxWidth: 360,alignment: .leading)
+        } else {
+            HStack {
+                Image(systemName: "person")
+                    .frame(maxWidth: 32, alignment: .top)
+                    .padding(.bottom, 16)
+                    .padding(.trailing, 4)
+                HStack {
+                    Text(message.text)
+                        .padding()
+                }
+                .frame(maxWidth: 250, alignment: .leading)
+                .background(.gray)
+                .cornerRadius(20)
+            }
+            .frame(maxWidth: 360,alignment: .leading)
+        }
+    }
+}
+
+#Preview {
+    MessageView(message: Message(uid: "123", text: "我是明日香", photoURL: "", createdAt: Date()))
+}
+
