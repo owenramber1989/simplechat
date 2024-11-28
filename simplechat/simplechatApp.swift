898149cfc58ec7afc14984f24c0f621e569be515
owenramber1989 <owenramber1989@163.com>
2024-11-28 21:12:45 +0800

    Initial Commit

---
+++ simplechat/simplechatApp.swift
@@ -0,0 +1,17 @@
+//
+//  simplechatApp.swift
+//  simplechat
+//
+//  Created by 张晋铭 on 2024/11/28.
+//
+
+import SwiftUI
+
+@main
+struct simplechatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ simplechatTests/simplechatTests.swift
@@ -0,0 +1,17 @@
+//
+//  simplechatTests.swift
+//  simplechatTests
+//
+//  Created by 张晋铭 on 2024/11/28.
+//
+
+import Testing
+@testable import simplechat
+
+struct simplechatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
