d4b38fac6bed255c23be602b5ef67b92eef1358b
owenramber1989 <owenramber1989@163.com>
2024-11-29 12:46:47 +0800

    feat: add sign in button

---
+++ simplechat/Views/SignInView.swift
@@ -0,0 +1,52 @@
+//
+//  SignInView.swift
+//  simplechat
+//
+//  Created by 张晋铭 on 2024/11/28.
+//
+
+import SwiftUI
+
+struct SignInView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Image("asuka")
+                .resizable()
+                .frame(maxWidth: 400, maxHeight: 700, alignment: .top)
+                .scaledToFill()
+                .clipped()
+            VStack(spacing: 10) {
+                Button {
+                    print("apple")
+                } label: {
+                    Text("Sign in with Apple")
+                        .padding()
+                        .foregroundColor(.primary)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke()
+                                .foregroundColor(.primary)
+                                .frame(width: 300)
+                        }
+                }
+                Button {
+                    print("google")
+                } label: {
+                    Text("Sign in with Google")
+                        .padding()
+                        .foregroundColor(.primary)
+                        .overlay {
+                            RoundedRectangle(cornerRadius: 20)
+                                .stroke()
+                                .foregroundColor(.primary)
+                                .frame(width: 300)
+                        }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    SignInView()
+}
